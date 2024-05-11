744193b6efdbcb211b56f61610350052aa8e401c
Philipp Maier <25420070+philmph@users.noreply.github.com>
2024-05-11 15:39:27 +0200

    Change to http.StatusCreated constant

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(201)
+	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("Save a new snippet..."))
 }
 

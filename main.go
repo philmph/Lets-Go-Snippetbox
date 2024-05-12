c26d3bbb93481696cbbb8feea721708577967f30
Philipp Maier <25420070+philmph@users.noreply.github.com>
2024-05-12 20:21:11 +0200

    Add a Server Header and use Fprintf to write to the http.ResponseWriter

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
 	w.Write([]byte("Hello from Snippetbox\n"))
 }
 
@@ -18,8 +19,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
-	w.Write([]byte(msg))
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {

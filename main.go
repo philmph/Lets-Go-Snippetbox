54bbef31531c5391cf573e67f9ccc15e23c7df26
Philipp Maier <25420070+philmph@users.noreply.github.com>
2025-06-09 21:13:03 +0200

    2.6.

--- main.go
+++ main.go
@@ -18,15 +18,19 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
-	w.Write([]byte(msg))
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
+
 	w.Write([]byte("Display a form for creating a new snippet..."))
 }
 
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
+	w.WriteHeader(http.StatusCreated)
+
 	w.Write([]byte("Save a new snippet..."))
 }
 

25565e57754344eff323ed79d1f72355d83cdac8
Philipp Maier <25420070+philmph@users.noreply.github.com>
2025-06-28 15:09:40 +0200

    5.2.

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/philmph/Lets-Go-Snippetbox/internal/models"
 
 type templateData struct {
-	Snippet models.Snippet
+	Snippet  models.Snippet
+	Snippets []models.Snippet
 }
